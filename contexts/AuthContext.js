a6ba27aefd101bef4675cf10c58f556f6b12371e
Aditya Ranjan <123758666+kvianAR@users.noreply.github.com>
2025-12-08 01:45:39 +0530

    Update AuthContext.js

--- contexts/AuthContext.js
+++ contexts/AuthContext.js
@@ -5,7 +5,7 @@ import { createContext, useContext, useState, useEffect } from 'react'
 const AuthContext = createContext()
 
 export function AuthProvider({ children }) {
-  const [ user , setUser] = useState(null)
+  const [user, setUser] = useState(null)
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
